1e671aaffc849f3d3d0bdf58a4e0567ef39e2301
Deep Patel <pateldeep691996@gmail.com>
2024-05-13 22:31:18 +0530

    update content for angular api and title.

--- src/api/services/person/get-person.service.ts
+++ src/api/services/person/get-person.service.ts
@@ -1,8 +1,8 @@
 class GetPersonService {
   public async Handler() {
-    const res =  {
-      PersonId: 1,
-      Name: 'This is something amazing',
+    const res = {
+      title: 'Fetched from api.',
+      content: 'Angular is a popular JavaScript framework for building dynamic web applications. It provides a structured way to develop client-side applications using TypeScript, a superset of JavaScript. With Angular, developers can create responsive, single-page applications (SPAs) with features such as data binding, dependency injection, and modular architecture.'
     };
     return res;
   }
